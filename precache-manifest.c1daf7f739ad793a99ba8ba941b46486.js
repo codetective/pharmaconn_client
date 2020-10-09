b0d942f2f6de46ae5909448620b7d72f52171013
codetective <iaminiodu@gmail.com>
2020-10-09 11:49:47 +0100

    initial build with auth routes, and context

---
+++ precache-manifest.c1daf7f739ad793a99ba8ba941b46486.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4859a5c034892a4b7503c489e296c478",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9ff83cf661da895a4a7",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "81fb3b27b1623fb85c14",
+    "url": "/static/css/main.b7bff262.chunk.css"
+  },
+  {
+    "revision": "d9ff83cf661da895a4a7",
+    "url": "/static/js/2.d5438fb4.chunk.js"
+  },
+  {
+    "revision": "885c0e205ef5e79f4aef448a818ba6a1",
+    "url": "/static/js/2.d5438fb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81fb3b27b1623fb85c14",
+    "url": "/static/js/main.b9b77766.chunk.js"
+  },
+  {
+    "revision": "2d2dba2503ba4b43402fdad5132f2053",
+    "url": "/static/js/main.b9b77766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "51c3f0c89a3c2d3293d8bdcd2246bbd6",
+    "url": "/static/media/Noto+Sans_400_normal.51c3f0c8.ttf"
+  },
+  {
+    "revision": "562c99547de538ff0ba6e31082d63f54",
+    "url": "/static/media/Noto+Sans_400_normal.562c9954.woff2"
+  },
+  {
+    "revision": "61aebdc436233a092d115fefaf291a7a",
+    "url": "/static/media/Noto+Sans_400_normal.61aebdc4.eot"
+  },
+  {
+    "revision": "8fc7779de799b69e404004702c4f9066",
+    "url": "/static/media/Noto+Sans_400_normal.8fc7779d.svg"
+  },
+  {
+    "revision": "cecb65bb1c8e0aca809a073069c0f797",
+    "url": "/static/media/Noto+Sans_400_normal.cecb65bb.woff"
+  }
+]);
